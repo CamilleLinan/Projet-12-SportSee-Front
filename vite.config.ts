8eaa72fe8b5948b3ac4d3b6e6ce1bb6814ea5b8b
Camille Linan <camille.linan@hotmail.com>
2024-01-31 21:40:43 +0100

    add sass + favicon + font

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@scss': new URL('src/styles/scss', import.meta.url).pathname
+    }
+  }
 })
